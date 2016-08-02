e1fc75588a891b9039da5860b119a9c39aa1a3f3
Lawrence Ling <Lawrence1995@hotmail.com>
2016-08-02 14:54:54 +1000

    clock.c compiles

---
+++ libs/libclock/src/clock.c
@@ -0,0 +1,27 @@
+#include <stdint.h>
+
+#include <clock/clock.h>
+
+int start_timer(seL4_CPtr interrupt_ep) {
+    return 0;
+}
+
+uint32_t register_timer(uint64_t delay, timer_callback_t callback, void *data) {
+    return 0;
+}
+
+int remove_timer(uint32_t id) {
+    return 0;
+}
+
+int timer_interrupt(void) {
+    return 0;
+}
+
+timestamp_t time_stamp(void) {
+    return 0;
+}
+
+int stop_timer(void) {
+    return 0;
+}
