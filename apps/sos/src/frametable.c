b0ae43958916c70fb2b021c3cc4712c51068b9e1
Lawrence Ling <Lawrence1995@hotmail.com>
2016-08-11 14:07:28 +1000

    Frame init in progress

--- apps/sos/src/frametable.c
+++ apps/sos/src/frametable.c
@@ -7,7 +7,9 @@
 
 #include "ut_manager/ut.h"
 
-#define FRAME_SIZE 1024lu /* Bytes */
+#include <sys/panic.h>
+
+#define FRAME_SIZE 1024llu /* Bytes */
 #define PAGE_SIZE 4096lu /* Bytes */
 
 static struct frame_entry {
@@ -26,23 +28,46 @@ static int free_index;
 static uint64_t base_addr;
 
 void frame_init(seL4_Word low, seL4_Word high) {
+    int err;
     uint64_t low64 = low;
     uint64_t high64 = high;
     
     uint64_t entry_size = sizeof(struct frame_entry);
     base_addr = (high64 * entry_size + PAGE_SIZE * low64) / (entry_size + PAGE_SIZE);
 
-    uint32_t frame_table_bits = 0;
     uint64_t frame_table_size = base_addr - low64;
-    while (frame_table_size >>= 1) frame_table_bits++;
 
-    //frame_table = malloc(frame_table_size);
-    frame_table = ut_alloc(10);
-    printf("f: %p\n", frame_table);
-    printf("div: %lu\n", frame_table_size / FRAME_SIZE);
-    for (uint64_t i = 0; i < frame_table_size / FRAME_SIZE; i++) {
-        seL4_Word *next = ut_alloc(10);
-        printf("n: %p\n", next);
+    /* Get num bits for frame_table */
+    uint32_t frame_table_bits = 0;
+    while (frame_table_size >>= 1) {
+        frame_table_bits++;
+    }
+    /* Reset frame_table_size value */
+    frame_table_size = base_addr - low64;
+
+    for (uint64_t i = 0; i <= frame_table_size / FRAME_SIZE; i++) {
+        seL4_Word *ft_addr;
+        seL4_CPtr cap;
+        if (i == 0) {
+            frame_table = ut_alloc(seL4_PageDirBits);
+            ft_addr = frame_table;
+        } else {
+            ft_addr = ut_alloc(seL4_PageDirBits);
+        }
+        err = cspace_ut_retype_addr(ft_addr,
+                                    seL4_ARM_PageDirectoryObject,
+                                    seL4_PageDirBits,
+                                    cur_cspace,
+                                    &cap);
+        conditional_panic(err, "Failed to allocate frame table cap");
+
+        /* Map to address space */
+        err = map_page(cap,
+                       seL4_CapInitThreadPD,
+                       ft_addr,
+                       seL4_AllRights,
+                       seL4_ARM_Default_VMAttributes);
+        conditional_panic(err, "Failed to map frame table");
     }
     free_index = 0;
 }
