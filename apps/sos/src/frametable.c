f9aceaa06a7e9f2d29f7e2ad8aa00aeb1b357fd9
Lawrence Ling <Lawrence1995@hotmail.com>
2016-08-12 15:31:06 +1000

    Now keeps track of frame table caps

--- apps/sos/src/frametable.c
+++ apps/sos/src/frametable.c
@@ -17,7 +17,12 @@ static struct frame_entry {
     /* 000 for 'valid' */
     //seL4_Word entry;
     seL4_CPtr cap;
-    int32_t next;
+    int32_t next_index;
+};
+
+static struct frame_table_cap {
+    seL4_CPtr cap;
+    struct frame_table_cap *next;
 };
 
 static struct frame_entry *frame_table;
@@ -26,7 +31,7 @@ static int32_t low_addr;
 static int32_t high_addr;
 static int free_index; /* -1 no free_list but memory is not full
                           -2 no free_list and memory is full (swapping later) */
-static seL4_CPtr frame_table_cap;
+static struct frame_table_cap *frame_table_cap_head;
 
 void frame_init(seL4_Word high,seL4_Word low) {
     int err;
@@ -46,28 +51,35 @@ void frame_init(seL4_Word high,seL4_Word low) {
     /* Allocated each section of frame table */
     seL4_Word ft_section_paddr;
     for (uint64_t i = 0; i < frame_table_size; i += PAGE_SIZE) {
+        seL4_CPtr cap;
 
         ft_section_paddr = ut_alloc(seL4_PageBits);
         seL4_Word ft_section_vaddr = i + PROCESS_VMEM_START;
-
-        /* Set pointer to head of frame_table */
-        if (i == 0) {
-            frame_table = ft_section_vaddr;
-        }
 
         err = cspace_ut_retype_addr(ft_section_paddr,
                                     seL4_ARM_SmallPageObject,
                                     seL4_PageBits,
                                     cur_cspace,
-                                    &frame_table_cap);
+                                    &cap);
         conditional_panic(err, "Failed to allocate frame table cap");     
 
-        err = map_page(frame_table_cap,
+        err = map_page(cap,
                        seL4_CapInitThreadPD,
                        ft_section_vaddr,
                        seL4_AllRights,
                        seL4_ARM_Default_VMAttributes);
         conditional_panic(err, "Failed to map frame table");
+
+        /* Set pointer to head of frame_table and keep track of caps */
+        struct frame_table_cap *cap_holder = malloc(sizeof(struct frame_table_cap));
+        cap_holder->cap = cap;
+        if (i == 0) {
+            frame_table = ft_section_vaddr;
+            cap_holder->next = NULL;
+        } else {
+            cap_holder->next = frame_table_cap_head;
+        }
+        frame_table_cap_head = cap_holder;
 
         /* Clear frame_table memory */
         memset(ft_section_vaddr, 0, PAGE_SIZE);
@@ -121,7 +133,7 @@ int32_t frame_alloc(seL4_Word *vaddr) {
     } else {
         ret_index = free_index;
         frame_vaddr = ((ret_index << 12) + base_addr - low_addr + PROCESS_VMEM_START);
-        free_index = frame_table[free_index].next;
+        free_index = frame_table[free_index].next_index;
     }
     
     memset(frame_vaddr, 0, PAGE_SIZE);
@@ -129,6 +141,6 @@ int32_t frame_alloc(seL4_Word *vaddr) {
 }
 
 void frame_free(int32_t index) {
-    frame_table[index].next = free_index;
+    frame_table[index].next_index = free_index;
     free_index = index;
 }
