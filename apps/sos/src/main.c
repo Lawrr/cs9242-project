f546c0a45f45e42dfc3a97c3059178d4e3ba0e95
Lawrence Ling <Lawrence1995@hotmail.com>
2016-08-12 14:15:06 +1000

    Added function with all the frame table tests

--- apps/sos/src/main.c
+++ apps/sos/src/main.c
@@ -417,6 +417,60 @@ static inline seL4_CPtr badge_irq_ep(seL4_CPtr ep, seL4_Word badge) {
     return badged_cap;
 }
 
+void frame_table_test() {
+    /* Allocate 10 pages and make sure you can touch them all */
+    printf("Test 1\n");
+    for (int i = 0; i < 10; i++) {
+        /* Allocate a page */
+        seL4_Word *vaddr;
+        frame_alloc(&vaddr);
+        assert(vaddr);
+
+        /* Test you can touch the page */
+        *vaddr = 0x37;
+        assert(*vaddr == 0x37);
+
+        printf("Page #%d allocated at %p\n",  i, (void *) vaddr);
+    }
+
+    /* Test that you never run out of memory if you always free frames. */
+    printf("Test 2\n");
+    for (int i = 0; i < 10000; i++) {
+        /* Allocate a page */
+        seL4_Word *vaddr;
+        int page = frame_alloc(&vaddr);
+        assert(vaddr != 0);
+
+        /* Test you can touch the page */
+        *vaddr = 0x37;
+        assert(*vaddr == 0x37);
+
+        /* print every 1000 iterations */
+        if (i % 1000 == 0) {
+            printf("Page #%d allocated at %p\n",  i, vaddr);
+        }
+
+        frame_free(page);
+    }
+
+    /* Test that you eventually run out of memory gracefully,
+       and doesn't crash */
+    printf("Test 3\n");
+    while (1) {
+        /* Allocate a page */
+        seL4_Word *vaddr;
+        frame_alloc(&vaddr);
+        if (!vaddr) {
+            printf("Out of memory!\n");
+            break;
+        }
+
+        /* Test you can touch the page */
+        *vaddr = 0x37;
+        assert(*vaddr == 0x37);
+    }
+}
+
 /*
  * Main entry point - called by crt.
  */
@@ -435,29 +489,14 @@ int main(void) {
     timer_init(epit1_vaddr, epit2_vaddr);
     seL4_CPtr timer_badge = badge_irq_ep(_sos_interrupt_ep_cap, IRQ_BADGE_TIMER);
     start_timer(timer_badge);
-
-    /* Test that you eventually run out of memory gracefully,
-       and doesn't crash */
-    while (1) {
-        /* Allocate a page */
-        seL4_Word vaddr;
-        
-        frame_alloc(&vaddr);
-        if (!vaddr) {
-            printf("Out of memory!\n");
-            break;
-        }
-        /* Test you can touch the page */
-        int *vvaddr = vaddr;
-        *vvaddr = 0x37;
-        assert(*vvaddr == 0x37);
-    }
     
     /* Initialise serial driver */
     serial_handle = serial_init();
 
     /* Start the user application */
     start_first_process(TTY_NAME, _sos_ipc_ep_cap);
+
+    frame_table_test();
 
     /* Wait on synchronous endpoint for IPC */
     dprintf(0, "\nSOS entering syscall loop\n");
