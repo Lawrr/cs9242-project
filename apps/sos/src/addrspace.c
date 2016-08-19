50925607181a19bf5b75be151a24c29138aac3f2
Lawrence Ling <Lawrence1995@hotmail.com>
2016-08-19 10:57:24 +1000

    Done addrspace stuff

--- apps/sos/src/addrspace.c
+++ apps/sos/src/addrspace.c
@@ -15,27 +15,36 @@ struct region {
 };
 
 struct app_addrspace *as_new() {
-//    seL4_Word as = malloc(sizeof(struct app_addrspace));
-//    if (as == NULL) {
-//        return NULL;
-//    }
-//    return as;
-    return NULL;
+    struct app_addrspace *as = malloc(sizeof(struct app_addrspace));
+    if (as == NULL) {
+        return NULL;
+    }
+    as->regions = NULL;
+    as->page_table = NULL;
+    return as;
 }
 
 int as_define_region(struct app_addrspace *as,
                      seL4_Word vroot,
                      seL4_Word size,
                      seL4_Word permissions) {
-//    struct region *new_region = malloc(sizeof(struct region));
-//    if (new_region == NULL) {
-//        return -1;
-//    }
-//    if (as->regions == NULL) {
-//        as->regions = new_region;
-//    } else {
-//
-//    }
+    struct region *new_region = malloc(sizeof(struct region));
+    if (new_region == NULL) {
+        return -1;
+    }
+
+    /* Init region */
+    new_region->vroot = vroot;
+    new_region->size = size;
+    new_region->permissions = permissions;
+
+    if (as->regions == NULL) {
+        as->regions = new_region;
+        new_region->next = NULL;
+    } else {
+        new_region->next = as->regions;
+        as->regions = new_region;
+    }
 
     return 0;
 }
