b250e750b9b8b82531aff694ceb61988d83d66f5
Lawrence Ling <Lawrence1995@hotmail.com>
2016-08-19 12:27:27 +1000

    Added header guards to .h files

--- apps/sos/src/addrspace.h
+++ apps/sos/src/addrspace.h
@@ -1,3 +1,6 @@
+#ifndef _ADDRSPACE_H_
+#define _ADDRSPACE_H_
+
 struct app_addrspace;
 struct region;
 
@@ -7,3 +10,5 @@ int as_define_region(struct app_addrspace *as,
                      seL4_Word vroot,
                      seL4_Word size,
                      seL4_Word permissions);
+
+#endif /* _ADDRSPACE_H_ */
--- apps/sos/src/frametable.h
+++ apps/sos/src/frametable.h
@@ -1,5 +1,10 @@
+#ifndef _FRAMETABLE_H_
+#define _FRAMETABLE_H_
+
 void frame_init();
 
 int32_t frame_alloc(seL4_Word *vaddr);
 
 void frame_free(seL4_Word page);
+
+#endif /* _FRAMETABLE_H_ */
