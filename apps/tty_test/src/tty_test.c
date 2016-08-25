848b69314a8f5c61f8ea96cb7eaadb9c33a94cc7
alanYi <245655664@qq.com>
2016-08-25 06:20:04 -0700

    Revert "testing\n"
    This reverts commit ad31dd7f1e692fd996eaad510120dafbd8735579.

--- apps/tty_test/src/tty_test.c
+++ apps/tty_test/src/tty_test.c
@@ -84,7 +84,7 @@ int main(void){
 
     do {
         printf("task:\tHello world, I'm\ttty_test!\n");
-        //pt_test();
+        pt_test();
         thread_block();
         // sleep(1);	// Implement this as a syscall
     } while(1);
