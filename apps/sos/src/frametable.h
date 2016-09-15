ef03820282abbd27ae9e395e8206f0d9e6525434
Lawrence Ling <Lawrence1995@hotmail.com>
2016-09-15 23:45:12 +1000

    Added prototype for get_app_cap

--- apps/sos/src/frametable.h
+++ apps/sos/src/frametable.h
@@ -3,6 +3,13 @@
 
 #include "addrspace.h"
 
+struct app_cap {
+    struct app_addrspace *addrspace;
+    seL4_Word uaddr;
+    seL4_CPtr cap;
+    struct app_cap *next;
+};
+
 void frame_init();
 
 int32_t frame_alloc(seL4_Word *vaddr);
@@ -13,14 +20,9 @@ seL4_CPtr get_cap(seL4_Word vaddr);
 
 int32_t insert_app_cap(seL4_Word vaddr, seL4_CPtr cap, struct app_addrspace *addrspace,seL4_Word uaddr);
 
+int32_t get_app_cap(seL4_Word vaddr, struct app_cap **cap_ret);
+
 int32_t swap_in(seL4_Word uaddr);
 int32_t swap_out();
-
-struct app_cap {
-    struct app_addrspace *addrspace;
-    seL4_Word uaddr;
-    seL4_CPtr cap;
-    struct app_cap *next;
-};
 
 #endif /* _FRAMETABLE_H_ */
